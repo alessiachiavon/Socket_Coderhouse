8b9bc12169973e01e8803ff48328945c8e08aa3e
alessiachiavon <alessia.chiavon@gmail.com>
2023-08-19 19:49:40 -0300

    ahora si!

--- app.js
+++ app.js
@@ -4,29 +4,40 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+const  memoria = require('./contenedores/memoria.js')
+const archivo = require('./contenedores/archivo.js')
+
+const productosApi = new memoria()
+const mensajesApi = new archivo('mensajes.json')
+
+
+//Configuración de Socket.IO
+io.on("connection", async socket => {
+   socket.emit("productos", productosApi.listarAll());
+
+   socket.on("newProduct", (product) => {
+        productosApi.guardar(product);
+        io.sockets.emit("productList", productosApi.listarAll())
+   })
+
+// carga inicial de mensajes
+socket.emit('mensajes', await mensajesApi.listarAll());
+
+// actualizacion de mensajes
+socket.on('nuevoMensaje', async mensaje => {
+    mensaje.fyh = new Date().toLocaleString()
+    await mensajesApi.guardar(mensaje)
+    io.sockets.emit('mensajes', await mensajesApi.listarAll());
+})
+
+
+})
+
+
+
 const PORT = 8080
-
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
-
-app.get("/", (req, res) => {
-    res.render("index", {title: "Aplicacion Socket IO"})
-});
-
 http.listen(PORT, () => {
     console.log(`Server in running on port ${PORT}.`)
 });
-
-
-io.on("connection", (socket) => {
-    console.log("Un cliente se ha conectado");
-
-    socket.on("mensaje", (data)=> {
-        console.log("Mensaje recibido: ", data);
-        io.emit("mensaje", data);
-    })
-
-    socket.on("disconnect", () => {
-        console.log("Un cliente se ha desconectado");
-    })
-})
